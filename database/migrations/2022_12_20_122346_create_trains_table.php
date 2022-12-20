ed4226cfabd10c9d0c25f3bf7c3620c4a34c07cd
Danielateresa <dany.t_87@hotmail.it>
2022-12-20 15:17:35 +0100

    Seeded database with records

--- database/migrations/2022_12_20_122346_create_trains_table.php
+++ database/migrations/2022_12_20_122346_create_trains_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 20);
+            $table->string('stazione_di_partenza', 50);
+            $table->string('stazione_di_arrivo', 50);
+            $table->string('orario_di_partenza', 10);
+            $table->string('orario_di_arrivo', 10);
+            $table->string('codice_treno', 20);
+            $table->tinyInteger('numero_carrozze', 20)->nullable();
+            $table->boolean('in_orario')->default(1)->nullable();
+            $table->boolean('cancellato')->default(0)->nullable();
             $table->timestamps();
         });
     }
--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Train;
 
 class TrainSeeder extends Seeder
 {
@@ -12,8 +14,19 @@ class TrainSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        //ciclo for per generare tot record nella tabella con i dati relativi
+        for ($i=0; $i < 10; $i++) {
+            $new_train = new Train();
+            $new_train->azienda = $faker->company();
+            $new_train->stazione_di_partenza = $faker->city();
+            $new_train->stazione_di_arrivo = $faker->city();
+            $new_train->codice_treno = $faker->bothify('???-#####');
+            $new_train->numero_carrozze = $faker->numberBetween(1, 20);
+            $new_train->in_orario = $faker->randomElement([true,false]);
+            $new_train->cancellato = $faker->numberBetween([si, no]);
+            $new_train->save();
+        }
     }
 }
