59bd8007dd49cf1335beeaced31084df8d4e6001
Danielateresa <dany.t_87@hotmail.it>
2022-12-20 17:54:20 +0100

    Added button and style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,11 +22,21 @@
 
         <div class="align-items-md-stretch">
             <div class="jumbo h-100 p-5 text-white">
-                <h2>Viaggia con noi</h2>
-                <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look.
-                    Then,
-                    mix and match with additional component themes and more.</p>
-                <button class="btn btn-danger" type="button">Example button</button>
+                <div class="row g-0">
+                    <div class="col-6 p-4">
+                        <h2>Viaggia con noi</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione quaerat accusamus veniam
+                            porro
+                            asperiores aperiam distinctio magni cum aliquam, ab deserunt, voluptate fugit quam
+                            recusandae rerum
+                            molestias exercitationem adipisci tenetur? Et beatae repellendus ut voluptas porro aperiam,
+                            adipisci
+                            blanditiis mollitia maxime. Placeat commodi dolores accusamus veniam eaque illum laborum
+                            exercitationem molestias ipsum.</p>
+                        <button class="btn btn-danger" type="button">Scopi di più</button>
+                    </div>
+                </div>
+
             </div>
         </div>
 
@@ -34,28 +44,33 @@
 
             @forelse($trains as $train)
             <div class="d-flex border-bottom pt-4">
-                <img src="https://assets.change.org/photos/4/ur/qz/PJurQzCeqciNvBO-800x800-noPad.jpg?1522795360" alt="">
+                <div class="left d-flex justify-content-between">
+                    <img src="https://assets.change.org/photos/4/ur/qz/PJurQzCeqciNvBO-800x800-noPad.jpg?1522795360"
+                        alt="">
 
-                <div class="info ps-4 pb-4">
-                    <h6><strong>Azienda: </strong>{{$train->azienda}}</h6>
-                    <h6><strong>Parte da: </strong>{{$train->stazione_di_partenza}}</h6>
-                    <h6><strong>Arrivo a: </strong>{{$train->stazione_di_arrivo}}</h6>
-                    <h6><strong>Ora partenza: </strong>{{$train->orario_di_partenza}}</h6>
-                    <h6><strong>Ora di arrivo: </strong>{{$train->orario_di_arrivo}}</h6>
-                    <h6><strong>Codice: </strong>{{$train->codice_treno}}</h6>
-                    <h6><strong>Numero carrozze: </strong>{{$train->numero_carrozze}}</h6>
-                    @if($train->in_orario == 1)
-                    <h6 class="text-success">Viaggia in orario</h6>
-                    @else
-                    <h6 class="text-danger">Previsto ritardo</h6>
-                    @endif
-                    @if($train->cancellato == 1)
-                    <h6 class="text-danger">Treno cancellato</h6>
-                    @else
-                    <h6 class="text-success">Partenza confermata</h6>
-                    @endif
+                    <div class="info ps-4 pb-4">
+                        <h6><strong>Azienda: </strong>{{$train->azienda}}</h6>
+                        <h6><strong>Parte da: </strong>{{$train->stazione_di_partenza}}</h6>
+                        <h6><strong>Arrivo a: </strong>{{$train->stazione_di_arrivo}}</h6>
+                        <h6><strong>Ora partenza: </strong>{{$train->orario_di_partenza}}</h6>
+                        <h6><strong>Ora di arrivo: </strong>{{$train->orario_di_arrivo}}</h6>
+                        <h6><strong>Codice: </strong>{{$train->codice_treno}}</h6>
+                        <h6><strong>Numero carrozze: </strong>{{$train->numero_carrozze}}</h6>
+                        @if($train->in_orario == 1)
+                        <h6 class="text-success">Viaggia in orario</h6>
+                        @else
+                        <h6 class="text-danger">Previsto ritardo</h6>
+                        @endif
+                        @if($train->cancellato == 1)
+                        <h6 class="text-danger">Treno cancellato</h6>
+                        @else
+                        <h6 class="text-success">Partenza confermata</h6>
+                        @endif
+                    </div>
                 </div>
+                <button class="acquista btn btn-danger ms-auto mt-auto mb-4" type="button">Acquista</button>
             </div>
+
             @empty
             <h3>No trains to show now</h3>
             @endforelse
